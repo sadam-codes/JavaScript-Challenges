94f494985c5b39b4b44ea49df43568f08f455756
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2025-04-01 22:48:19 +0500

    functions in js

--- functions.js
+++ functions.js
@@ -1,3 +1,50 @@
 //IIFI
 ((name = "Ali") => { console.log(`Hello ${name}`) })
     ("sadam")
+
+
+// Write a function to check if a number is even or odd.
+const checkNumber = (num) => {
+    return num % 2 === 0 ? `${num} is Even` : `${num} is Odd`
+}
+console.log(checkNumber(3))
+
+// 3. Write a function to reverse a string.
+
+const reverseDtring = (str) => {
+    return str.split("").reverse().join("")
+}
+console.log(reverseDtring("Ali"))
+
+const checkPalondrom = (str) => {
+    let reverse = str.split("").reverse().join("")
+    return str === reverse
+};
+console.log(checkPalondrom("HeH"))
+
+// find sumof Al
+const sumOfAll = (num) => {
+    return num.reduce((a, b) => {
+        return (a + b)
+    }, 0)
+}
+console.log(sumOfAll([2, 3, 4]))
+
+//  Write a function to check if an array contains a specific element.
+const includes = (num, element) => {
+    return num.includes(element)
+}
+console.log(includes([2, 3, 4], 3))
+
+//  Write a function to generate a random number between two numbers.
+const generateNumbers = (num1, num2) => {
+    return Math.random() * (num2 - num1 + 1) + num1
+}
+console.log(generateNumbers(1, 2))
+
+//  Write a function to remove duplicates from an array.
+const removeDuplicates = (num)=>{
+    return [... new Set(num)]
+}
+console.log(removeDuplicates([3,4,5,3,4,4,6]))
+
